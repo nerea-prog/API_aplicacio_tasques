9e5fc996f7130eb5015933252fbf4197f86d7cd3
nerea-prog <narvaeznerea18@gmail.com>
2026-02-02 17:25:19 +0100

    Estructura del proyecte - dto fet

--- projecte1/src/main/java/com/ra12/projecte1/dto/TaskRequestDTO.java
+++ projecte1/src/main/java/com/ra12/projecte1/dto/TaskRequestDTO.java
@@ -1,4 +1,60 @@
 package com.ra12.projecte1.dto;
 
+import java.sql.Timestamp;
+
 public class TaskRequestDTO {
+    private String title, category, imagePath;
+    private boolean completed;
+
+    public TaskRequestDTO() {
+    }
+
+    public TaskRequestDTO(String title, String category, String imagePath, boolean completed) {
+        this.title = title;
+        this.category = category;
+        this.imagePath = imagePath;
+        this.completed = completed;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    @Override
+    public String toString() {
+        return "TaskRequestDTO{" +
+                "title='" + title + '\'' +
+                ", category='" + category + '\'' +
+                ", imagePath='" + imagePath + '\'' +
+                ", completed=" + completed +
+                '}';
+    }
 }
--- projecte1/src/main/java/com/ra12/projecte1/dto/TaskResponseDTO.java
+++ projecte1/src/main/java/com/ra12/projecte1/dto/TaskResponseDTO.java
@@ -1,4 +1,92 @@
 package com.ra12.projecte1.dto;
 
+import java.sql.Timestamp;
+
 public class TaskResponseDTO {
+    private long id;
+    private String title, category, imagePath;
+    private boolean completed;
+    private Timestamp dataCreated, dataUpdated;
+
+    public TaskResponseDTO() {
+    }
+
+    public TaskResponseDTO(long id, String title, String category, String imagePath, boolean completed, Timestamp dataCreated, Timestamp dataUpdated) {
+        this.id = id;
+        this.title = title;
+        this.category = category;
+        this.imagePath = imagePath;
+        this.completed = completed;
+        this.dataCreated = dataCreated;
+        this.dataUpdated = dataUpdated;
+    }
+
+    public Timestamp getDataUpdated() {
+        return dataUpdated;
+    }
+
+    public void setDataUpdated(Timestamp dataUpdated) {
+        this.dataUpdated = dataUpdated;
+    }
+
+    public Timestamp getDataCreated() {
+        return dataCreated;
+    }
+
+    public void setDataCreated(Timestamp dataCreated) {
+        this.dataCreated = dataCreated;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "TaskResponseDTO{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", category='" + category + '\'' +
+                ", imagePath='" + imagePath + '\'' +
+                ", completed=" + completed +
+                ", dataCreated=" + dataCreated +
+                ", dataUpdated=" + dataUpdated +
+                '}';
+    }
 }
